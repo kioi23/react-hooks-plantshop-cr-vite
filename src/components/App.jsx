970a0f3affcf67e69705ab3591bbe6ec99c5b203
kioi23 <kioijgithinji23@gmail.com>
2026-05-11 16:21:17 +0300

    reorder component imports and adjust layout in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,40 @@
 import { useEffect, useState } from "react"
+import Header from "./components/Header"
+import Search from "./components/Search"
+import PlantList from "./components/PlantList"
+import NewPlantForm from "./components/NewPlantForm"
 
 function App() {
+  // Stores all plants from backend
   const [plants, setPlants] = useState([])
+
+  // Stores user search input
   const [search, setSearch] = useState("")
 
+  // Fetch plants when page loads
   useEffect(() => {
     fetch("http://localhost:6001/plants")
       .then((response) => response.json())
       .then((data) => setPlants(data))
   }, [])
 
+  // Adds new plant into state
   function handleAddPlant(newPlant) {
     setPlants([...plants, newPlant])
   }
 
+  // Filters plants using search input
   const filteredPlants = plants.filter((plant) =>
     plant.name.toLowerCase().includes(search.toLowerCase())
   )
 
   return (
     <main>
-      <Search search={search} setSearch={setSearch} />
+      <Header />
 
       <NewPlantForm onAddPlant={handleAddPlant} />
+
+      <Search search={search} setSearch={setSearch} />
 
       <PlantList plants={filteredPlants} />
     </main>
