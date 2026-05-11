9edad5db8130af8c66e6a4719419a8335c27c705
kioi23 <kioijgithinji23@gmail.com>
2026-05-11 16:18:18 +0300

    refactor App component structure and enhance plant filtering functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,32 @@
-import React from "react";
-import Header from "./Header";
-import PlantPage from "./PlantPage";
-import { useState, useEffect } from "react";
-
+import { useEffect, useState } from "react"
 
 function App() {
   const [plants, setPlants] = useState([])
-  
-  return (
-    <div className="app">
-      <Header />
-      <PlantPage />
-    </div>
-  );
-}
-function handleAddPlant(newPlant) {
-  setPlants([...plants, newPlant])
-}
-<NewPlantForm onAddPlant={handleAddPlant} />
+  const [search, setSearch] = useState("")
 
-export default App;
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((response) => response.json())
+      .then((data) => setPlants(data))
+  }, [])
+
+  function handleAddPlant(newPlant) {
+    setPlants([...plants, newPlant])
+  }
+
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(search.toLowerCase())
+  )
+
+  return (
+    <main>
+      <Search search={search} setSearch={setSearch} />
+
+      <NewPlantForm onAddPlant={handleAddPlant} />
+
+      <PlantList plants={filteredPlants} />
+    </main>
+  )
+}
+
+export default App
