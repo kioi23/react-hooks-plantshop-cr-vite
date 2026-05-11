f9a9c9361ce9d495e844d70f589f276a6e900ac3
kioi23 <kioijgithinji23@gmail.com>
2026-05-11 16:01:42 +0300

    fix PlantList component to correctly render plant cards from props

--- src/components/PlantList.jsx
+++ src/components/PlantList.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
-  <PlantList plants={plants} /> 
+function PlantList({ plants }) {
   return (
-    <ul className="cards">{<PlantList plants={plants} />}</ul>
-  );
+    <ul className="cards">
+      {plants.map((plant) => (
+        <PlantCard key={plant.id} plant={plant} />
+      ))}
+    </ul>
+  )
 }
 
 export default PlantList;
